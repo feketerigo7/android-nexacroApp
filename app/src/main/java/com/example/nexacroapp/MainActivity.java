cbca608c35991750415648bd97debf57d6256364
Kim-JaeHwan <jaehwan.kim@tobesoft.com>
2021-06-03 16:55:18 +0900

    update aar project settings

--- app/src/main/java/com/example/nexacroapp/MainActivity.java
+++ app/src/main/java/com/example/nexacroapp/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.nexacroapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.nexacro.NexacroResourceManager;
+import com.nexacro.NexacroUpdatorActivity;
+
+public class MainActivity extends NexacroUpdatorActivity {
+
+    public MainActivity() {
+        super();
+
+        setProjectURL("http://your project url");
+        setBootstrapURL("http://your bootstrap url/start_android.json");
+
+        // Default startupClass is NexacroActivity.class for NexacroView
+        // setStartupClass(UserCustomActivity.class);
+
+        /* for extends NexacroActivity (for soZam module, archive 없이 구동)*/
+        //NexacroResourceManager.createInstance(this);
+        //NexacroResourceManager.getInstance().setBootstrapURL("http://your bootstrap url/start_android.json", true);
+        //NexacroResourceManager.getInstance().setProjectURL("http://your project url");
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        // updatetype : server 인 경우에만 아래 주석제거, 기본값은 false 임(update / local )
+        //NexacroResourceManager.createInstance(this);
+        //NexacroResourceManager.getInstance().setDirect(true);
+
+        Intent intent = getIntent();
+        if(intent != null) {
+            String bootstrapURL = intent.getStringExtra("bootstrapURL");
+            String projectUrl = intent.getStringExtra("projectUrl");
+            if(bootstrapURL != null) {
+                setBootstrapURL(bootstrapURL);
+                setProjectURL(projectUrl);
+            }
+        }
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if (!isTaskRoot()) {
+            intent = getIntent();
+            if (intent.hasCategory(Intent.CATEGORY_LAUNCHER) && Intent.ACTION_MAIN.equals(intent.getAction())) {
+                startRuntime();
+                finish();
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
     }
 }
